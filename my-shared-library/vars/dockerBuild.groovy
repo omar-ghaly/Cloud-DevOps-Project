a10da6f57675e87ba844f0a4502562d52c0d6a9d
omar-ghaly <omarghaly2156@gmail.com>
2025-11-21 19:24:16 +0200

    update

--- my-shared-library/vars/dockerBuild.groovy
+++ my-shared-library/vars/dockerBuild.groovy
@@ -1,13 +1,10 @@
-#!/usr/bin/env groovy
-
 def call(String imageName, String imageTag = "latest", String dockerfilePath = "./docker") {
     echo "Building Docker image: ${imageName}:${imageTag} from ${dockerfilePath}"
-
     try {
-        // لازم تستخدم docker داخل `steps` context في Jenkins Pipeline
-        sh "docker build -t ${imageName}:${imageTag} ${dockerfilePath}"
+        // Use the pipeline's 'docker' object
+        def dockerImage = script.docker.build("${imageName}:${imageTag}", dockerfilePath)
         echo "Successfully built ${imageName}:${imageTag}"
-        return true
+        return dockerImage
     } catch (Exception e) {
         echo "Failed to build Docker image: ${e.message}"
         throw e
